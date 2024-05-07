a334b221c3190668e752864ee0efaca0da49ee73
theabhishek <callabhimishra@gmail.com>
2024-05-07 16:52:12 +0530

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -69,7 +69,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-   # 'whitenoise.middleware.WhiteNoiseMiddleware',
+     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
   
 ]
 
